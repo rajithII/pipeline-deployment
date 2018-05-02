d05e55bb2ccf26a4df7cdfaa66fa3df162840dee
rajithII <30283449+rajithII@users.noreply.github.com>
2018-05-02 19:42:05 +0530

    Update deploy-stack.sh

--- deploy-stack.sh
+++ deploy-stack.sh
@@ -4,5 +4,5 @@ bucketname=$(echo "$LambdaName" | tr '[:upper:]' '[:lower:]')
 echo "bucketname:$bucketname"
 sed -i -e 's/Bucketname/'"$bucketname"'/g' CodePipelineTemplate.yaml
 aws s3 cp CodePipelineTemplate.yaml s3://aadhri-test-buck/
-aws cloudformation create-stack --stack-name $LambdaName-Stack --template-url https://s3.us-east-2.amazonaws.com/aadhri-test-buck/CodePipelineTemplate.yaml --capabilities CAPABILITY_IAM 
+aws cloudformation create-stack --stack-name $LambdaName-Pipeline-Stack --template-url https://s3.us-east-2.amazonaws.com/aadhri-test-buck/CodePipelineTemplate.yaml --capabilities CAPABILITY_IAM 
 
