6d915a4a5882e6ee8a35089a6818058440558646
rajithII <30283449+rajithII@users.noreply.github.com>
2018-05-02 17:03:30 +0530

    Update deploy-stack.sh

--- deploy-stack.sh
+++ deploy-stack.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sed -i -e 's/LambdaName/'"$LambdaName"'/g' CodePipelineTemplate.yaml
-aws s3 cp $Template_Name s3://$S3_bucket/
-
+aws s3 cp CodePipelineTemplate.yaml s3://aadhri-test-buck/
+aws cloudformation create-stack --stack-name $LambdaName-Stack --template-url https://s3.us-east-2.amazonaws.com/aadhri-test-buck/CodePipelineTemplate.yaml --capabilities CAPABILITY_IAM 
 
